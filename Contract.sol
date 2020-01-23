54e38af510e8dcd721fa585c16e3f9315c191ea1
Amélie DUVERNET <amelieonline@gmail.com>
2020-01-23 13:45:27 +0100

    Fin contrat

--- Contract.sol
+++ Contract.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -12,8 +14,18 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    public function createZombie(string _name, uint _dna) {
-        zombies.push(Zombie(_name, _dna));
+    function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        NewZombie(id, _name, _dna);
     }
 
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 }
